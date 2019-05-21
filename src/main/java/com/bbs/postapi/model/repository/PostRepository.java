5ee0c49103cc44feae7291dd285b8f3a1aaed706
thekingofcity <wzl7777777@gmail.com>
2019-05-21 20:34:34 +0800

    adding automated tests

--- src/main/java/com/bbs/postapi/model/repository/PostRepository.java
+++ src/main/java/com/bbs/postapi/model/repository/PostRepository.java
@@ -18,7 +18,7 @@ public interface PostRepository extends ReactiveMongoRepository<Post, String> {
     Flux<Post> findTopAllPagination(String community, final Pageable page);
 
     // https://docs.spring.io/spring-data/mongodb/docs/current/reference/html/#mongodb.repositories.queries.sort
-    @Query(value = "{ 'parentId': {$ne:null},'deleted': false }", sort = "{ 'lastUpdateTime': -1}")
+    @Query(value = "{ 'parentId': {$ne:null}, 'deleted': false }", sort = "{ 'lastUpdateTime': -1}")
     Flux<Post> findHotAllPagination(final Pageable page);
 
     @Query("{ 'parentId': null, 'deleted': false, 'author': ?0 }")
